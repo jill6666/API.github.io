4162a817d01a39e633851275811abc00d3e31046
Jill <73696750+jill6666@users.noreply.github.com>
2021-02-17 07:39:04 +0800

    fetchCountriesData

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+const GetCountriesData = () => {
+    const [country, setCountry] = useState('Taiwan');
+    const [people, setPeople] = useState(23000000);
+    const [language, setLanguage] = useState('Chinese');
+    const [currencies, setCurrencies] = useState([]);
+    const [alpha3Code, setAlpha3Code] = useState('twn');
+    
+    // fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //   const countryElement = data[0];
+    //   setCountry(()=>countryElement.name);
+    //   setPeople(()=>countryElement.population);
+    //   setAlpha3Code(()=>countryElement.alpha3Code.toLowerCase());
+    //   setLanguage(()=>countryElement.capital);
+    //   setCurrencies(()=>countryElement.currencies.code);
+    //   });
+
+    const fetchCountriesData = (props) => {
+      fetch(`https://restcountries.eu/rest/v2/name/${props}`)
+      .then((response) => response.json())
+      .then((data, language, currencies) => {
+        //console.log('data === ', data);
+        const countryElement = data[0];
+        console.log('currenciesData === ', data[0]);
+        setCountry(()=>countryElement.name);
+        setPeople(()=>countryElement.population);
+        setAlpha3Code(()=>countryElement.alpha3Code.toLowerCase());
+        setLanguage(()=>countryElement.capital);
+        setCurrencies(()=>countryElement.currencies.code);
+        });
+    };
+
+    
+     
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="country">
+    <img className="country__img" src={`https://restcountries.eu/data/${alpha3Code}.svg`} />
+    <div className="country__data">
+      <h3 className="country__name">{country}</h3>
+      <button onClick={() => fetchCountriesData('taiwan')}>
+        Taiwan
+      </button>
+      <button onClick={() => fetchCountriesData('japan')}>
+        Japan
+      </button>
+      <button onClick={() => fetchCountriesData('canada')}>
+        Canada
+      </button>
+      <button onClick={() => fetchCountriesData('usa')}>
+        USA
+      </button>
+      <h4 className="country__region"></h4>
+      <p className="country__row"><span>👫</span>{people}</p>
+      <p className="country__row"><span>🗣️</span>{language}</p>
+      <p className="country__row"><span>💰</span>{currencies}</p>
     </div>
-  );
-}
+  </div>
+    </div>
+  );}
 
-export default App;
+
+export default GetCountriesData;
